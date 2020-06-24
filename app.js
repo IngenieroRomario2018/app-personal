7b7cc0352a97a080c67dfe32ee0791290e6aeed3
IngRomario <ingenieroromario@gmail.com>
2020-06-24 20:45:23 +0200

    iniciar

---
+++ app.js
@@ -0,0 +1,8 @@
+function registrar() {
+
+    var email = document.getElementById('email').Value;
+    var login = document.getElementById('user').Value;
+    console.log(email);
+    console.log(login);
+
+}
